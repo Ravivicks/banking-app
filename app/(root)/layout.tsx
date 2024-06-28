f6f13373d3d4eed9ab96238ce3d3366254a85804
Ravi Anuragi <anuragiravi85@gmail.com>
2024-06-28 17:59:45 +0530

    final

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const loggedIn = await getLoggedInUser();
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      {children}
+    </main>
+  );
+}
