bcf8df22336db0a4ec6f96358ae36b4790f667cf
Ravi Anuragi <anuragiravi85@gmail.com>
2024-06-29 07:55:59 +0530

    new changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,17 +8,17 @@ export default async function RootLayout({
   return (
     <main className="flex min-h-screen w-full justify-between font-inter">
       {children}
-      {/* <div className="auth-asset">
+      <div className="auth-asset">
         <div>
           <Image
-            src="/icons/auth-image.svg"
+            src="/icons/bg.png"
             alt="Auth image"
-            width={500}
-            height={500}
+            width={700}
+            height={1000}
             className="rounded-l-xl object-contain"
           />
         </div>
-      </div> */}
+      </div>
     </main>
   );
 }
